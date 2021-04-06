09e4ab7907c975e5c11687de37d9184d771b2225
zanderson1998 <zanderson1998@yahoo.com>
2021-04-06 15:22:05 -0600

    finish writing queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,30 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
-        }
+        // get all thoughts or thought by username
+        thoughts: async (parent, {username}) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        //get thought by id
+        thought: async (parent, {_id}) => {
+            return Thought.findOne({ _id });
+        },
+        //get all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+        //get user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
     }
 };
 
