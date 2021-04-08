dc206874c371d966e558ceb209fa55d464f65d94
zanderson1998 <zanderson1998@yahoo.com>
2021-04-08 16:52:41 -0600

    add muliple pages functionality and also include routers

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,15 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//import page components
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 const client = new ApolloClient({
   uri: '/graphql'
 });
@@ -15,14 +24,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/profile/:username?' component={Profile} />
+              <Route exact path='/thought/:id' component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </ApolloProvider>
+      </Router>
+    </ApolloProvider >
   );
 }
 
